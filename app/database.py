16a5c223a9b56b08cfebc78189150a1e8807f675
SulimanKh87 <157065889+SulimanKh87@users.noreply.github.com>
2025-11-04 09:09:10 +0200

    Add MongoDB async connection using Motor client

---
+++ app/database.py
@@ -0,0 +1,6 @@
+# app/database.py
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.config import settings
+
+client = AsyncIOMotorClient(settings.mongodb_uri)
+db = client[settings.mongodb_db]
